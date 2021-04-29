38bdf83528d40bd3bd8a8d291109538e5129acce
ssuryaashokkumar <ssuryaashokkumar@gmail.com>
2021-04-29 17:59:06 +0000

    add file with displaying lattiude and longitude

--- ip_address_details.py
+++ ip_address_details.py
@@ -1,7 +1,17 @@
 import ipapi
+import json
 
-
-
-data=ipapi.location()
-
-print(data)
+dataip=input("Enter the IP address:")
+data=ipapi.location(ip=dataip)
+# dataipai=json.loads(data)
+# dataipai=json.dumps(data,indent=1)
+# print(dataipai)
+print("IP Address       : "+str(data['ip']))
+print("Version          : "+str(data['version']))
+print("City             : "+str(data['city']))
+print("Region           : "+str(data['region']))
+print("Country          : "+str(data['country_code'])+"|"+str(data['country_name']))
+print("Postal           : "+str(data['postal']))
+print("European Union   : "+str(data['in_eu']))
+print("Latitude         : "+str(data['latitude']))
+print("Longitude        : "+str(data['longitude']))
