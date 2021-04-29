32b5d9869d84e66df1ac95c4e8f40f656ace93db
sandeepsrisiva <sandeep.b@aec.edu.in>
2021-04-29 17:46:26 +0000

    Add a new application file name is ipa_address_detail.py

--- ip_address_details.py
+++ ip_address_details.py
@@ -1,8 +1,7 @@
 import ipapi
-import json
 
-dataip=input("Enter the IP address:")
-data=ipapi.location(ip=dataip)
-# dataipai=json.loads(data)
-dataipai=json.dumps(data,indent=1)
-print(dataipai)
+
+
+data=ipapi.location()
+
+print(data)
