dae65be13b3b49ef0cd87788686ae0b7bfc0a1b2
DeepakVegda <deepak.c.vegda@gmail.com>
2021-04-29 17:16:37 +0000

    Addfile ip_address_detail.py, Modification: When Enter the IP the IP details are come

--- ip_address_details.py
+++ ip_address_details.py
@@ -1,8 +1,8 @@
 import ipapi
 import json
 
-
-data=ipapi.location()
+dataip=input("Enter the IP address:")
+data=ipapi.location(ip=dataip)
 # dataipai=json.loads(data)
 dataipai=json.dumps(data,indent=1)
 print(dataipai)
